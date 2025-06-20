33f4fabf375211cdb0208f007777a2a53343144a
Wagner Fonseca <eng.sotware.fonseca@gmail.com>
2025-06-19 22:02:30 -0300

    Adicionando rota de login

--- app.py
+++ app.py
@@ -96,5 +96,8 @@ def login():
     return render_template('login.html')
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
